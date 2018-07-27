db4a12b6cd16d59abd3defd12a9fa0e60be01313
Kamry Bowman <kamry.bowman@gmail.com>
2018-07-27 11:06:48 -0600

    Add get actionsById functionality

--- server.js
+++ server.js
@@ -7,6 +7,18 @@ const makeRouter = require('./middlewares/makeRouter');
 
 const server = express();
 
+server.get('/api/projects/:id/actions', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const payload = await projectModel.getProjectActions(Number(id));
+    res.status(200).json(payload);
+  } catch {
+    res.status(500).json('Database was not able to fulfill this request. Please ensure you are requesting actions' +
+  'of an existing project');
+  }
+
+  
+})
 server.use('/api/actions', makeRouter('project', 'name, and description, and completed', actionModel));
 server.use('/api/projects', makeRouter('action', 'project_id, description, notes, and completed', projectModel));
 
